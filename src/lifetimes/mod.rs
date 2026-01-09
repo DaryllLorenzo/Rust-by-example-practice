124d1bf9d1566f2a3ae8d411973c664e75a6b401
DaryllLorenzo <vikingwarrior4444@gmail.com>
2026-01-09 08:40:41 -0500

    Ex6: added use case of nested lifetimes

--- src/lifetimes/mod.rs
+++ src/lifetimes/mod.rs
@@ -5,7 +5,7 @@ impl Lifetimes{
         println!("------------- Excercise 6 Lifetimes ---------------");
         let book1 = Book::new("Book#1", "author#1");
         let user1 = User::new("Matt");
-        let book2 = Book::new("Book#2", "author#2");
+        let book2 = Book::new("Book#223", "author#2");
         let b1 = Borrow::new(&book1, &user1);
         let b2 = Borrow::new(&book2, &user1);
         b1.show_data();
@@ -13,6 +13,11 @@ impl Lifetimes{
         let borrows = vec![&b1,&b2];
         let user_reference = Lifetimes::find_borrowed_book(borrows, "Book#1");
         println!("User_reference_data: {:?}", user_reference);
+
+        println!("Referencia de libro {:?}", Lifetimes::longest_borrow(&b1, &b2));
+
+        let r1 = Renovation::new(&b1, "2026-01-25");
+        r1.show_data();
     }
 
     fn find_borrowed_book<'a>(borrows: Vec<&'a Borrow>, book_name: &str) -> &'a User{
@@ -27,7 +32,17 @@ impl Lifetimes{
     
     fn foo(book: Book) -> () {
         println!("{:?}", book);
-    } 
+    }
+
+    // references of results doesn't outlive original borrows
+    fn longest_borrow<'a>(b1: &'a Borrow, b2: &'a Borrow) -> &'a Book{
+        
+        if b1.book.title >= b2.book.title {
+            return b1.book; 
+        }
+
+        b2.book
+    }
 
 }
 
@@ -43,6 +58,7 @@ struct User {
     name : String
 }
 
+#[derive(Debug)]
 struct Borrow<'a> {
     book : &'a Book,
     user : &'a User
@@ -76,5 +92,36 @@ impl<'a> Borrow<'a> {
     fn show_data(&self) -> () {
         println!("Book: {:?}", self.book);
         println!("User: {:?}", self.user);
+    }
+}
+
+// we need two lifetimes bc, Renovation will have its own lifetime in scope, so we need to tell to the compiler 
+// that the Renovation lives at most as the Borrow, so 'a <= 'b, and the lifetime 'b would be the lifetime of the own Borrow,
+// which specifies to Book and User how long the references are going to live.
+
+// 'a: lifetime of the REFERENCE to the Borrow
+// 'b: lifetime of the Borrow itself (and thus of the Book/User it references)
+//
+// The key relationship: 'a <= 'b
+// (The reference cannot outlive the data it points to)
+//
+// This creates a chain: Renovation('a) <= Borrow('b) <= (Book, User)
+struct Renovation<'a, 'b>{
+    // lives for at least 'a
+    borrow: &'a Borrow<'b>, // has its own lifetime 'b
+    new_date: String
+}
+
+impl<'a, 'b> Renovation<'a, 'b> {
+    fn new(borrow: &'a Borrow<'b>, new_date: &str) -> Self{
+        Renovation { 
+            borrow : borrow,
+            new_date : new_date.to_string()
+        }
+    }
+
+    fn show_data(&self) -> () {
+        println!("Borrow: {:?}", self.borrow);
+        println!("New_date: {:?}", self.new_date);
     }
 }
